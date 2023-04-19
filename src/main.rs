d1e6adc7801fefd99114baf255cc4a52d1315d8c
Tobshub <tobanigeorge@gmail.com>
2023-04-19 07:01:21 +0100

    help message and path resolving

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::path::PathBuf;
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let command: Option<&str> = args.get(1).map(|x| &**x);
+    if command.is_some() {
+        let command = command.unwrap();
+        match command {
+            "--upload" | "-u" => {
+                for path in &args[2..] {
+                    let abs_path = fs::canonicalize(PathBuf::from(path));
+                    match abs_path {
+                        Ok(path) => println!("Uploading file at {:?}", path),
+                        Err(_) => println!("Could not find file: {}", path),
+                    }
+                }
+            }
+            "--help" | "-h" | _ => display_help_message(),
+        }
+    }
+}
+
+fn display_help_message() {
+    println!(
+        "\
+Tobsmg CLI - v2.0.0
+
+Usage:
+
+    --server <server-url>       Configure the remote server url
+    -u, --upload                Upload files to the server
+    -t, --temp-upload          Temporarily Upload files to the server 
+    "
+    );
+}
